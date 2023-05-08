87ebcb90db1e16a3003b60958c7f9502704abc51
CoreJa <i@coreja.com>
2023-05-08 09:53:10 -0400

    finalize

--- task9/js/model.js
+++ task9/js/model.js
@@ -57,11 +57,10 @@ class Model{
     loadTexture(tex_url) {
         const texture = gl.createTexture();
         gl.bindTexture(gl.TEXTURE_2D, texture);
-        gl.texImage2D(gl.TEXTURE_2D, 0 ,gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0, 255, 0, 255]));
+        // gl.texImage2D(gl.TEXTURE_2D, 0 ,gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0, 255, 0, 255]));
 
         const image = new Image();
         image.onload = function() {
-            // gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
             gl.bindTexture(gl.TEXTURE_2D, texture);
             gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
